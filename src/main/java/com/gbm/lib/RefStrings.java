dc0af29a39dcaffe51f45c295e5facc62fcfb06b
Chickn-man <85531727+Chickn-man@users.noreply.github.com>
2021-10-03 11:41:56 -0400

    lib

---
+++ src/main/java/com/gbm/lib/RefStrings.java
@@ -0,0 +1,9 @@
+package com.gbm.lib;
+
+public class RefStrings {
+	public static final String MODID = "gbm";
+	public static final String NAME = "Gooberish Mod";
+	public static final String VERSION = "0.2.3";
+	public static final String CLIENTSIDE = "com.gbm.main.ClientProxy";
+	public static final String SERVERSIDE = "com.gbm.main.ServerProxy";
+}
